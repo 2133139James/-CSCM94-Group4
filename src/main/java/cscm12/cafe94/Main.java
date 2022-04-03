6cbda2347fe5da742f03b775243c007fb057d55c
2133139James <92098338+2133139James@users.noreply.github.com>
2022-04-03 18:35:13 +0100

    Update Main.java

--- src/main/java/cscm12/cafe94/Main.java
+++ src/main/java/cscm12/cafe94/Main.java
@@ -6,13 +6,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.util.Objects;
 
-/**[Main]
+/**
  * Main place where the stage starts to branch out, the login page (root). *
  * @author Sumi Sunuwar
  * @version 1.0*/
 public class Main extends Application {
 
-    /**[start]
+    /**start
      * Main place stage, this is used as the beginning page.
      * Can be used to plug in the direct FXML page if you don't want to login all the time.
      * @param stage */
