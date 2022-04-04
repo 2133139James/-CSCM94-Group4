6de119e1a791c0a204e144cf8e0eab7946b726b2
PaulieNor <64064864+PaulieNor@users.noreply.github.com>
2022-04-04 18:47:41 +0100

    Added inheritance for tickets.

--- Cafe94/src/main/java/cscm12/cafe94/DriverTicket.java
+++ Cafe94/src/main/java/cscm12/cafe94/DriverTicket.java
@@ -5,70 +5,17 @@ package cscm12.cafe94;
  * @author Paul Norman
  * @version 1.0
  */
-public class DriverTicket {
+public class DriverTicket extends Ticket {
 
     private int orderID;
-    private String main;
-    private String side;
-    private String drink;
     private String deliveryAddress;
     private String driverName;
 
-    public DriverTicket(int orderID, String main, String side,
-                        String drink, String deliveryAddress, String driverName) {
+    public DriverTicket(int orderID, String main, String side, String drink,
+                        String deliveryAddress, String driverName) {
+        super(main, side, drink);
         this.orderID = orderID;
-        this.main = main;
-        this.side = side;
-        this.drink = drink;
         this.deliveryAddress = deliveryAddress;
-        this.driverName = driverName;
-    }
-
-    public int getOrderID() {
-        return orderID;
-    }
-
-    public void setOrderID(int orderID) {
-        this.orderID = orderID;
-    }
-
-    public String getMain() {
-        return main;
-    }
-
-    public void setMain(String main) {
-        this.main = main;
-    }
-
-    public String getSide() {
-        return side;
-    }
-
-    public void setSide(String side) {
-        this.side = side;
-    }
-
-    public String getDrink() {
-        return drink;
-    }
-
-    public void setDrink(String drink) {
-        this.drink = drink;
-    }
-
-    public String getDeliveryAddress() {
-        return deliveryAddress;
-    }
-
-    public void setDeliveryAddress(String deliveryAddress) {
-        this.deliveryAddress = deliveryAddress;
-    }
-
-    public String getDriverName() {
-        return driverName;
-    }
-
-    public void setDriverName(String driverName) {
         this.driverName = driverName;
     }
 }
--- Cafe94/src/main/java/cscm12/cafe94/KitchenTicket.java
+++ Cafe94/src/main/java/cscm12/cafe94/KitchenTicket.java
@@ -1,21 +1,16 @@
 package cscm12.cafe94;
 
-public class KitchenTicket {
+public class KitchenTicket extends Ticket {
 
     private int referenceNumber;
     private String orderType;
-    private String main;
-    private String side;
-    private String drink;
     private boolean isCooked;
 
-    public KitchenTicket(int referenceNumber, String orderType, String main, String side, String drink,
-                         boolean isCooked){
-        this.referenceNumber =referenceNumber;
+    public KitchenTicket(int referenceNumber, String main, String side,
+                         String drink, String orderType, boolean isCooked) {
+        super(main, side, drink);
+        this.referenceNumber = referenceNumber;
         this.orderType = orderType;
-        this.main = main;
-        this.side= side;
-        this.drink = drink;
         this.isCooked = isCooked;
     }
 
@@ -23,47 +18,23 @@ public class KitchenTicket {
         return referenceNumber;
     }
 
-    public String getOrderType() {
-        return orderType;
-    }
-
-    public String getMain() {
-        return main;
-    }
-
-    public String getSide(){
-        return side;
-    }
-
-    public String getDrink(){
-        return drink;
-    }
-
-    public boolean getIsCooked() {
-        return isCooked;
-    }
-
     public void setReferenceNumber(int referenceNumber) {
         this.referenceNumber = referenceNumber;
+    }
+
+    public String getOrderType() {
+        return orderType;
     }
 
     public void setOrderType(String orderType) {
         this.orderType = orderType;
     }
 
-    public void setMain(String main) {
-        this.main = main;
-    }
-
-    public void setSide(String side) {
-        this.side = side;
-    }
-
-    public void setDrink(String drink) {
-        this.drink = drink;
-    }
-
     public boolean isCooked() {
         return isCooked;
+    }
+
+    public void setCooked(boolean cooked) {
+        isCooked = cooked;
     }
 }
