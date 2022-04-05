df71225fcc93948740b8848fc2ad60a5c066c085
PaulieNor <64064864+PaulieNor@users.noreply.github.com>
2022-04-05 07:41:10 +0100

    Takeaway Collection connected

--- Cafe94/src/main/java/cscm12/cafe94/TakeawayCollectionController.java
+++ Cafe94/src/main/java/cscm12/cafe94/TakeawayCollectionController.java
@@ -3,7 +3,9 @@ package cscm12.cafe94;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -11,7 +13,9 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 import static cscm12.cafe94.StaffLoginController.staffSession;
@@ -62,6 +66,16 @@ public class TakeawayCollectionController implements Initializable {
         WaiterTicket ticket = takeawayTickets.getItems().get(index);
         ticket.collect();
         getTicketTable();
+    }
+    /**
+     Switches to Waiter page.
+     @param event is to trigger fxml swap */
+    public void switchToWaiter(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Waiter.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @Override
